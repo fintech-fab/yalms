459dd6a1dd3cc6e520a43b2de10c1694f9a5da8c
IgorVPetrov <info@igoruha.org>
2015-02-09 00:56:47 +0400

    добавление столбцов в таблицы user_student и user_teacher

---
+++ app/database/migrations/2015_02_08_145041_add_columns_to_user_teacher_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUserTeacherTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('user_teacher', function(Blueprint $table)
+		{
+			// где и на кого учился
+			$table->text('education');
+            // кем и сколько работал до начала преподавательской деятельности
+			$table->text('job_before');
+			// работает ли сейчас и если да, то кем
+			$table->text('job_now');
+			// проходил ли, и если проходил, то где, специальное обучение на преподавателя
+			$table->text('teacher_training');
+			// преподавательский стаж
+			$table->integer('teaching_experience');
+			// методика обучения
+			$table->text('training_technique');
+			// требования к студенту
+			$table->text('requirements_to_student');
+			// всё что дополнительно хочет сообщить о себе
+			$table->text('additional_info');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user_teacher', function(Blueprint $table)
+		{
+			$table->dropColumn('education', 'job_before', 'job_now', 'teacher_training',
+				'teaching_experience', 'training_technique', 'requirements_to_student',
+				'additional_info');
+		});
+	}
+
+}
---
+++ app/database/migrations/2015_02_08_200319_add_columns_to_user_student_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUserStudentTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('user_student', function(Blueprint $table)
+		{
+			// где и на кого учился
+			$table->text('education');
+			// кем и сколько работал
+			$table->text('job_before');
+			// работает ли сейчас и если да, то кем
+			$table->text('job_now');
+			// пожелания по методике обучения
+			$table->text('wishes_by_a_training_technique');
+			// всё что дополнительно хочет сообщить о себе
+			$table->text('additional_info');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('user_student', function(Blueprint $table)
+		{
+			$table->dropColumn('education', 'job_before', 'job_now',
+				'wishes_by_a_training_technique', 'additional_info');
+		});
+	}
+
+}
